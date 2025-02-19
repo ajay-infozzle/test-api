4ef2dfc4a1332b1db59a088a13bd5ba8aad4a1f3
ajay-infozzle <ajay.m@infozzle.com>
2025-02-19 12:47:20 +0530

    link updated

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.get('/p/:shortId', async (req, res) => {
     const title = "Test Dynamic Linking";
     const description = "This is test api for dynamic link";
     const  image = "https://images.unsplash.com/photo-1739219959019-dd317f76c7e8?q=80&w=2058&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"; 
-    const originalUrl = "http://192.168.0.115:3000/p/postid";
+    const originalUrl = "https://test-dynamic-link.onrender.com/p/postid";
 
     res.send(`
         <html>
@@ -35,7 +35,7 @@ app.get('/p/:shortId', async (req, res) => {
             <meta property="og:title" content="${title}">
             <meta property="og:description" content="${description}">
             <meta property="og:image" content="${image}">
-            <meta property="og:url" content="http://192.168.0.115:3000/p/${req.params.shortId}">
+            <meta property="og:url" content="https://test-dynamic-link.onrender.com/p/${req.params.shortId}">
             <meta property="og:type" content="article">
             <title>${title}</title>
         </head>
